bc1f13c2df26e01dd00ba8f3d7b0862b298327b5
pratyushmb <pratyushmb@gmail.com>
2020-06-02 17:03:27 -0500

    avoiding the conflict of directories for flask, templates and static directories recreated within the project.

--- Ineuron/prat_calculator/calc.py
+++ Ineuron/prat_calculator/calc.py
@@ -12,6 +12,33 @@ calc = Flask(__name__)  # created Flask app
 def index_page():
     return render_template('index_page.html')  # inbuilt flask method to showcase this web page into browser
 
+@calc.route('/math', methods=['POST'])  # this will be called from UI
+def math_operation():
+    if request.method == 'POST':
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        result_val = ''
+
+        if operation == 'add':
+            r = num1 + num2
+            result_val = "the sum of " + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+
+        if operation == 'subtract':
+            r = num1 - num2
+            result_val = "the difference of " + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+
+        if operation == 'multiply':
+            r = num1 * num2
+            result_val = "the product of " + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+
+        if operation == 'divide':
+            r = num1 % num2
+            result_val = "the quotient when " + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+
+        return render_template('resultset.html', result_val=result_val)
+
+
 @calc.route('/via_postman', methods=['POST'])  # calling the API from POSTMAN
 def math_operation_via_postman():
     if request.method == 'POST':
